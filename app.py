756adc3ca99536d695ebc85619eacad8939774e6
abhishekrajdhar <rajdhardubey4434@gmail.com>
2025-03-15 00:06:49 +0530

    new

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from src.helper import download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone  # ✅ Corrected Pinecone import
-from langchain_openai import OpenAI
+from langchain_community.vectorstores import Pinecone  # ✅ Corrected Pinecone import
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
